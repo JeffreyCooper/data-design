654afe211ba6d4257723b50e7559404b91d673b3
Jeffrey Cooper <jcooper37@cnm.edu>
2017-04-13 10:35:35 -0600

    Upload epic revisions 05.

--- epic/use-case.php
+++ epic/use-case.php
@@ -13,21 +13,24 @@
 			<h1>Use Case</h1>
 		</header>
 		<main>
-			<h2>Ellen wants to find a crafting idea to create a gift for a friend's birthday.</h2>
+			<h2>Ellen wants to buy a Cat Condo as a gift for a friend's birthday.</h2>
 			<h3>User Story:</h3>
-			<p>As a user, I want to learn how to build a cat condo from instructions on the website.</p>
+			<p>As a user, I want to buy a cat condo from www.verybadetsy.com.</p>
 				<ol>
-					<li>Search for Cat Condo Design</li>
-					<li>Click to view Cat Condo Design</li>
+					<li>Search for Cat Condo</li>
+					<li>Click to view Very Bad Cat Condo</li>
+					<li>Click to favorite Very Bad Cat Condo</li>
+					<li></li>
 				</ol>
 			<<h3>Interaction Flow</h3>
 				<ol>
 					<li>User enters Cat Condo Design into Search Bar.</li>
-					<li>Site returns search results of Cat Condo Design.</li>
-					<li>User clicks on Cat Condo Design.</li>
-					<li>Site transfers user to page Cat Condo Design.</li>
+					<li>Site returns search results of Cat Condo.</li>
+					<li>User clicks on Very Bad Cat Condo.</li>
+					<li>Site transfers user to page Very Bad Cat Condo.</li>
+					<li>User clicks on favorite button.</li>
+					<li>Site logs favorite for Very Bad Cat Condo by User</li>
 				</ol>
-
 		</main>
 	</body>
 </html>
