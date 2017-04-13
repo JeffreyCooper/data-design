8293b6b544454c9b732bc23f9a53eaa426efd61d
Jeffrey Cooper <jcooper37@cnm.edu>
2017-04-13 11:09:07 -0600

    Upload epic revisions 06

--- epic/persona.php
+++ epic/persona.php
@@ -18,45 +18,32 @@
 			<img src="../images/ellen-nicol.jpg" alt="Ellen Nicol" />
 			<p>Ellen Nicol is a 59 year old Grandmother and Seamstress living in Albuquerque, NM.
 			</p>
-
 			<h3>Ellen Nicol's Profile</h3>
-			<ul>
-				<li>Hometown: Albuquerque, NM</li>
-				<li>Occupation: Seamstress</li>
-				<li>Age: 59</li>
-				<li>Hobbies: Quilting, Sewing, Church, Grand-children.</li>
-				<li>Favotie Foods: Nutrition Bars; Enchiladas; Tuna</li>
-				<li>Relationship Status: Married for 40 years.</li>
-				<li>Annual Income: 80K to 100K</li>
-				<li>Automobile: Toyota Rav4</li>
-				<li>Past Occupations: Teacher, Secretary</li>
-			</ul>
-
-
-
-			<h3>Cats &amp; Dogs</h3>
-			<table>
-				<tr>
-					<th>Cats</th>
-					<th>Dogs</th>
-				</tr>
-				<tr>
-					<td>Tabby</td>
-					<td>Weiner</td>
-				</tr>
-				<tr>
-					<td>Tortoise</td>
-					<td>Labrador</td>
-				</tr>
-				<tr>
-					<td>Sheba</td>
-					<td>Dori</td>
-				</tr>
-				<tr>
-					<td>Meow</td>
-					<td>Ruff</td>
-				</tr>
-			</table>
+			<h4>General</h4>
+				<ul>
+					<li>Hometown: Albuquerque, NM</li>
+					<li>Occupation: Seamstress</li>
+					<li>Age: 59</li>
+					<li>Past Occupations: Teacher, Secretary</li>
+					<li>Relationship Status: Married for 40 years.</li>
+				</ul>
+			<h4>Interests</h4>
+				<ul>
+					<li>Hobbies: Quilting, Sewing, Church, Grand-children.</li>
+					<li>Favotie Foods: Nutrition Bars; Enchiladas; Tuna</li>
+				</ul>
+			<h4>Supplemental Data</h4>
+				<ul>
+					<li>Annual Income: 80K to 100K</li>
+					<li>Automobile: Toyota Rav4</li>
+				</ul>
+			<h4>Technology Data</h4>
+				<ul>
+					<li>Home Computer: HP Desktop PC Windows 7</li>
+					<li>Phone: iPhone 5</li>
+					<li>Home Network: Xfinity</li>
+					<li>Mobile Network: Verizon</li>
+				</ul>
 		</main>
 	</body>
 </html>
